cf1657e75de0c192d7c370f432235aeb074b5baa
munakaghamelu <mgodwinaghamelu@yahoo.co.uk>
2026-08-22 16:15:03 +0100

    frontend deisgn changes

---
+++ client/src/Logo.tsx
@@ -0,0 +1,10 @@
+function Logo() {
+  return (
+    <div className="brand-mark" aria-label="Visio Former">
+      <img src="/logo.png" alt="" />
+      <span>visio former</span>
+    </div>
+  )
+}
+
+export default Logo
